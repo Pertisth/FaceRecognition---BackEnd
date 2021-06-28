b48c9fd4001a2ebfcb0f613c74a488ed63f2b297
Pertisth Singh <pertisthsingh@gmail.com>
2021-06-28 23:46:31 +0530

    DataBase NodeJS bug Fixed

--- server.js
+++ server.js
@@ -18,8 +18,10 @@ const appClarifai = new Clarifai.App({
 const db = knex({
     client: 'pg',
     connection:{
-        connectionString:process.env.DATABASE_URL,
-        ssl :true
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
